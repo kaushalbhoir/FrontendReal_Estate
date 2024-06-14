2e33a0b1e5fdc8c4049afe3d33f952df2e9552fe
kaushal bhoir <samikshabhoir43@gmail.com>
2024-06-14 15:23:45 +0530

    before

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -4,30 +4,55 @@ import "./slider.scss"
 
 function Slider({images}) {
    const[imageIndex,setImageIndex]= useState(null);
+   const changeSlide=(direction)=> {
+       if(direction==="left"){
+         if(imageIndex===0){
+              setImageIndex(images.length-1);
+         }
+         else {
+                 setImageIndex(imageIndex-1)
+         }
+      }
+         else if(direction==="right"){
+           if(imageIndex===images.length-1){
+                setImageIndex(0);
+           }
+           else{
+             setImageIndex(imageIndex+1);
+           }
+            
+         }
+
+       }
+   
    return(
       <div className="slider">
          {imageIndex!==null && (
         <div className="fullslider">
-                 <div className="arrow">
+                 <div className="arrow" onClick={()=>changeSlide("left")}>
                      <img src="./arrow.png" alt="" />
                  </div>
                  <div className="imgContainer">
-                    <img src={images[0]} alt="" />
+                    <img src={images[imageIndex]} alt="" />
                  </div>
-                 <div className="arrow">
+                 <div className="arrow"  onClick={()=>changeSlide("right")}   >
                      <img src="./arrow.png" className="right" alt="" />
                  </div>
-                 <div className="close">
-                    x
+                 <div className="close" onClick={()=>setImageIndex(null)}>
+                   X 
                  </div>
         </div>
-        ) }
+         )}
         <div className="bigImage">
-            <img src={images[0]}  onClick={()=>setImageIndex(0)}      alt="" />
+            <img src={images[0]} 
+             onClick={()=>setImageIndex(0)} 
+                  alt="" />
         </div>
          <div className="smallImages">
              {images.slice(1).map((image,index)=>(
-               <img src ={image}   onClick={()=> setImageIndex(index)}     alt="" key={index}/>
+               <img src ={image} 
+                 onClick={()=> setImageIndex(index+1)}   
+                   alt="" key={index}/>
             ))}
          </div>
       </div>
--- src/routes/singlepage/singlePage.jsx
+++ src/routes/singlepage/singlePage.jsx
@@ -1,4 +1,5 @@
 import "./singlePage.scss";
+import Map from "../../components/map/Map"
 import SLider from "../../components/slider/slider";
 import { singlePostData, userData } from "../../lib/dummydata";
 
@@ -29,7 +30,95 @@ function SinglePage() {
         </div>
       </div>
       <div className="features">
-        <div className="wrapper">{/* Add features here if needed */}</div>
+        <div className="wrapper">
+          <p className="title">General</p> 
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Renter is Responsibe</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Pet policy</span>
+                <p>Pets Allowed</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Property fees</span>
+                <p>Must have 3x the rent in total household income</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">
+            Sizes
+            </p> 
+             <div className="sizes">
+          <div className="size">
+
+              <img src="/size.png" alt="" />
+              <span>80sqft</span>
+             </div>
+             <div className="size">
+              <img src="/size.png" alt="" />
+              <span>2 beds</span>
+             </div>
+             <div className="size">
+              <img src="/size.png" alt="" />
+              <span>1 Bathroom</span>
+             </div>
+          </div>
+          <p className="title">
+             NearBy Places
+            </p> 
+          <div className="listHorizontal">
+          <div className="feature">
+              <img src="/school.png" alt="" />
+              <div className="featureText">
+                <span>School</span>
+                <p>250m away</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Bus stop</span>
+                <p>100m away</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Restuarant</span>
+                <p>200m away</p>
+              </div>
+            </div>
+            
+            
+
+          </div>
+          <p className="title">Location</p> 
+          <div className="mapContainer">
+            <Map items={[singlePostData]}/>
+          </div>
+           <div className="buttons">
+              <button>
+              <img src="/chat.png" alt="" />
+              Send a message
+              </button>
+              <button>
+              <img src="/save.png" alt="" />
+               Save the Place
+              </button>
+
+             
+           </div>
+          </div>
       </div>
     </div>
   );
